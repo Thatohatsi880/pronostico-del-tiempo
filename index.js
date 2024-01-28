a94354a2492dc57e91f48975e1d93a2fd9466c6b
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-01-28 21:29:52 +0200

    injected weather data

--- index.js
+++ index.js
@@ -2,13 +2,43 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-  
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement= document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data * 1000);
+
+
+    descriptionElement.innerHTML= response.data.condition.description;
     cityElement.innerHTML = response.data.city;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML= `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = `${date.getDate} ${date.getHours()}:${date.getMinutes()}`;
     temperatureElement.innerHTML = Math.round(temperature);
   }
   
+  function formateDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", 
+                "Monday", 
+                "Tuesday", 
+                "Wednesday", 
+                "Thursday", 
+                "Friday", 
+                "Saturday"
+            ];
+            let day = days[date.getDay()];
+
+            if(minutes < 10){
+             minutes =`0${minutes}`   
+            }
+
+            return `${day} ${hours}:${minutes}`;
+
+  }
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "254367t9e3bc6o9a7f0bab9ff26930f7";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
   }

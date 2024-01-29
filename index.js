a08e6b69d2034e463da50f8e2712ce9f3d8819d9
Thatohatsi Matshidiso Tilodi <thatohatsitilodi@gmail.com>
2024-01-29 07:29:00 +0200

    Made changes to the URL Key on JavaScript file

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function refreshWeather(response) {
   }
   
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "254367t9e3bc6o9a7f0bab9ff26930f7";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
   }
